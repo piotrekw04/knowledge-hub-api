78d8df3706e9627be4ef2c8ef49503b2f6a53125
piotrekw04 <piotr.wojtala04@gmail.com>
2026-01-10 21:19:50 +0100

    Initial setup: FastAPI, Docker, Postgres, SQLAlchemy, Alembic

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from sqlalchemy import text
+from app.db.session import engine
+
+# To się wykonuje RAZ przy starcie aplikacji
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("STARTUJĘ APLIKACJĘ")
+    try:
+        # Próbujemy połączyć się z bazą
+        async with engine.begin() as conn:
+            await conn.execute(text("SELECT 1"))
+        print("SUKCES! Połączono z bazą danych PostgreSQL.")
+    except Exception as e:
+        print(f"BŁĄD! Nie można połączyć z bazą: {e}")
+    yield
+    print("--- ZAMYKAM APLIKACJĘ ---")
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/")
+def read_root():
+    return {"status": "ok"}
---
+++ app/models/document.py
@@ -0,0 +1,15 @@
+import uuid
+from datetime import datetime
+from app.db.base import Base
+from sqlalchemy import Text, String, DateTime, UUID
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.sql import func
+
+
+class Document(Base):
+    __tablename__ = "documents"
+
+    id: Mapped[uuid.UUID] = mapped_column(UUID, primary_key=True, default=uuid.uuid4)
+    filename: Mapped[str] = mapped_column(String(255))
+    content: Mapped[str] = mapped_column(Text())
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
